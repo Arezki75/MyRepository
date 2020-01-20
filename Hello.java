09efeab1a5effbe73f5f7b873e12044f5478ec2b
Arezki TAHDJERIT <kikinfo06@yahoo.fr>
2020-01-20 16:49:21 +0100

    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+  // Définition de la fonction main
+  public static void main(String[] arg){
+    // Affichage du commentaire Hello dans la fenêtre de commande
+    System.out.println("Hello");
+  }
+}
